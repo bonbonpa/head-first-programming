8168ad068cbbed500d7f67bc97bb14ce676c1e40
bonbonpa <sphinspf@gmail.com>
2017-03-03 15:45:02 +0000

    add randint for random number

--- 2.loop/rewrite-game.py
+++ 2.loop/rewrite-game.py
@@ -1,12 +1,16 @@
+from random import randint
+secret = randint(1,10)
 print("Wellcome!")
 guess = 0
-while guess != 5:
+while guess != secret:
     g = input("Guess the number: ")
     guess = int(g)
-    if guess > 5:
+    if guess > secret:
         print("You High")
-    else: 
+    elif guess < secret:
         print("You low")
+    else: 
+        print("You Correct")
     #print("You lose!")
 print("You win!")
 print("Game Over!")
